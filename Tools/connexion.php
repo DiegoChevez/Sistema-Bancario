9f522a355bbef85924272b45e064fc59b55bca26
DiegoChevez <diegochevez144@gmail.com>
2023-03-14 00:38:33 -0600

    Creacion del archivo de conexion con mysql

---
+++ Tools/connexion.php
@@ -0,0 +1,17 @@
+<?php 
+    
+
+    class mysqlConex{
+        
+        public function conn(){
+            $host = "localhost";
+            $user = "root";
+            $passwd = "";
+            $bd = "banco_perla";
+            $con = mysqli_connect($host,$user,$passwd,$bd);
+            if(!$con){
+                die("!MISTAKE! - The connection has not been established");
+            }
+            return $con;
+        }
+    }
