e750aa8d4c8885cc4afd8db264b7b13b846a31de
CesarGonzalez2601 <cesareliasrodas26@gmail.com>
2023-05-12 20:43:08 -0600

    Agregamos la plantilla completa

---
+++ Checker/nav.php
@@ -0,0 +1,43 @@
+<aside id="left-panel" class="left-panel">
+    <nav class="navbar navbar-expand-sm navbar-default">
+        <div id="main-menu" class="main-menu collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="menu-title">Banco Perla</li><!-- /.menu-title -->
+                <li>
+                    <a href="index.php"><i class="menu-icon fa fa-home"></i> &nbsp; Inicio </a>
+                </li>
+                <li>
+                    <a href="../Website/index.html"><i class="menu-icon fa fa-globe"></i>&nbsp; SiteWeb</a>
+                </li>
+                <li class="menu-title">Acciones</li><!-- /.menu-title -->
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-user"></i>&nbsp; Usuarios</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="fa fa-users "></i><a href="users-clients.php">&nbsp; Ver Usuarios</a></li>
+                        <li><i class="fa fa-plus"></i><a href="customer-creator.php">&nbsp; Nuevo Usuario</a></li>
+                    </ul>
+                </li>
+
+                <li class="menu-item-has-children dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-user-md"></i>&nbsp; Dependientes</a>
+                    <ul class="sub-menu children dropdown-menu">
+                        <li><i class="fa fa-th"></i><a href="users-partners.php">&nbsp; Ver Dependientes</a></li>
+                        <li><i class="fa fa-plus"></i><a href="partners-creator.php">&nbsp; Nuevo Dependientes</a></li>
+                    </ul>
+                </li>
+
+                <li class="">
+                    <a href="customer-lending.php"><i class="menu-icon fa fa-money"></i>&nbsp; Prestamos</a>
+                </li>
+
+                <li class="menu-title">Opciones</li><!-- /.menu-title -->
+                <li class="">
+                    <a href="sessiondestroy.php"><i class="menu-icon fa fa-retweet"></i>&nbsp; Cambiar de Cuenta</a>
+                </li>
+                <li class="">
+                    <a href="sessiondestroy.php"><i class="menu-icon fa fa-sign-out"></i>&nbsp; Salir</a>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </nav>
+</aside>
